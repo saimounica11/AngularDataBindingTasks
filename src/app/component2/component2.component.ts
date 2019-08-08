1f3f68bc107a3cc6cd78567dcd8ed5f81ab7a55f
saimounica11 <nssmounica18@gmail.com>
2019-08-08 10:59:36 +0530

    Child Should Display Text in GrandParent

---
+++ src/app/component2/component2.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+  @Output() public parentObject=new EventEmitter();
+  public sendMessage(message){
+    this.parentObject.emit(message);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
